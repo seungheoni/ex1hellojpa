6710ef24c62124a64531dd4e4a50eb79c912a3ce
ued123 <32861341+ued123@users.noreply.github.com>
2023-01-23 14:45:22 +0900

    양방향 연관관계와 객체와 db 차이에서 나오는 딜레마

--- src/main/java/hellojpa/Team.java
+++ src/main/java/hellojpa/Team.java
@@ -1,9 +1,8 @@
 package hellojpa;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class Team {
@@ -14,6 +13,9 @@ public class Team {
     private Long id;
 
     private String name;
+
+    @OneToMany(mappedBy = "team") //mappedBy 반대편에 대한 필드 매핑 이름이 뭔지. 해당 mappedby가 걸린 필드는 읽기만 가능.
+    private List<Member> members = new ArrayList<>();
 
     public Long getId() {
         return id;
@@ -29,5 +31,13 @@ public class Team {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public List<Member> getMembers() {
+        return members;
+    }
+
+    public void setMembers(List<Member> members) {
+        this.members = members;
     }
 }
