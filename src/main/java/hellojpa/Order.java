459d71b971fd85e33aa864118f3e40d5ab135c05
ued123 <32861341+ued123@users.noreply.github.com>
2023-01-29 14:01:58 +0900

    1:N , N:1, 1:1, N:M 매핑 연습

---
+++ src/main/java/hellojpa/Order.java
@@ -0,0 +1,20 @@
+package hellojpa;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "ORDERS")
+public class Order {
+
+    @Id @GeneratedValue
+    @Column(name = "ORDER_ID")
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "MEMBER_ID")
+    private Member member;
+
+    @OneToOne
+    @JoinColumn(name = "DELIVERY_ID")
+    private Delivery delivery;
+}
